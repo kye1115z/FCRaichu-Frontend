09a6e531fa81be96e2248c543a7e425c7fdecfeb
kye1115z <kye1115z@naver.com>
2026-03-06 15:30:09 +0900

    TODO: 절대 경로 설정하기

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// TODO:  절대경로 설정하기
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      //
+    ],
+  },
+});

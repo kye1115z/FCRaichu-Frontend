323cb6dbeef1f3808e343cae4f01aaef2815338e
YeEun Kim <kye1115z@naver.com>
2026-03-09 03:01:55 +0900

    #2 feat: 직관 기록 페이지 - 내용 작성 컴포넌트 구현

--- src/components/RecordWriteStep.tsx
+++ src/components/RecordWriteStep.tsx
@@ -1,8 +1,82 @@
+import React, { useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
+interface FormData {
+  gameId: number;
+  title: string;
+  content: string;
+  ticketImage: string;
+  images: File[];
+}
+
+// 부모로부터 context로 받아온 데이터 활용하기
+interface PostContext {
+  selectedDate: number;
+  ticketImage: string;
+}
+
+// TODO: 날짜 데이터를 어떻게 받아올지?
 export default function RecordWriteStep() {
+  const navigation = useNavigate();
+  // 경기 데이터
+  const { selectedDate, ticketImage } = useOutletContext<PostContext>();
+
+  // 직관 기록 post 시 보낼 데이터 정의
+  const [formData, setFormData] = useState<FormData>({
+    gameId: selectedDate, // context로 받아온 경기 데이터
+    title: "",
+    content: "",
+    ticketImage: ticketImage, // context로 받아온 티켓 이미지 데이터
+    images: [],
+  });
+
+  // textarea = content 입력 받기
+  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setFormData({
+      ...formData,
+      content: e.target.value,
+    });
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      title: e.target.value,
+    });
+  };
+
+  // image = images를 file 배열로 입력 받기
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    if (!file) return;
+
+    setFormData({
+      ...formData,
+      images: [...formData.images, file],
+    });
+  };
+
+  // TODO: 제출 로직 작성하기
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+
+    navigation("/");
+  };
   return (
-    <div>
-      <textarea>직관 기록 textarea</textarea>
-      <button>Send</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="제목" onChange={handleTitleChange} />
+      <textarea onChange={handleTextAreaChange}></textarea>
+      {/* TODO: 이미지 여러 개 */}
+      {/* TODO: 이미지 프리뷰 */}
+      <input
+        type="file"
+        accept="image/jpg, image/jpeg, image/png"
+        name="ticket_image"
+        onChange={handleImageChange}
+      />
+      <button type="submit">Send</button>
+    </form>
   );
 }

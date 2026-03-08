2cfdf006de9b7a824f3d8b26d14492dd4b2ab644
YeEun Kim <kye1115z@naver.com>
2026-03-08 20:21:56 +0900

    #2 setting: msw 기초 세팅(worker)

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,27 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    {/* APP 대신 RouterProvider 넣어줘야 한다. */}
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+// MSW 활성화 함수 정의
+async function enableMocking() {
+  // 개발 환경에서만 MSW가 동작하도록 설정해줌.
+  // vite 방식으로 현재 환경이 development인지 확인
+  if (import.meta.env.MODE !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser.ts");
+
+  return worker.start({
+    onUnhandledRequest: "bypass",
+  });
+}
+
+// MSW가 준비도니 후에 앱 렌더링 시작하도록
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      {/* APP 대신 RouterProvider 넣어줘야 한다. */}
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+});

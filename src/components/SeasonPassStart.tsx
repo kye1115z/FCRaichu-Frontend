50f1a12ed72bee39482a1ad0b5df1d84b06052c3
YeEun Kim <kye1115z@naver.com>
2026-03-09 03:11:24 +0900

    #2 feat: 직관 기록 페이지 - 시즌권 유저

--- src/components/SeasonPassStart.tsx
+++ src/components/SeasonPassStart.tsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 export default function SeasonPassStart() {
+  const navigation = useNavigate();
   return (
     <div>
       <h1>시즌권 포스트 작성</h1>
-      <button>Next</button>
+      <button onClick={() => navigation("/post/season-pass/new")}>Next</button>
     </div>
   );
 }

34f8fa7e6e17872d752dd43b8bb5cf0c775f9828
YeEun Kim <kye1115z@naver.com>
2026-03-08 21:51:34 +0900

    #2 refactory: SignUp handleChange 함수로 코드 간소화

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,20 +1,31 @@
-import { postSingUp } from "@/apis/auth/SignUp";
+import { postSingUp } from "@/apis/auth/postSignUp";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [formData, setFormData] = useState({
+  const navigate = useNavigate();
+
+  const [signUpData, setSignUpData] = useState({
     userId: "",
     password: "",
     nickname: "",
   });
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignUpData({
+      ...signUpData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await postSingUp(formData);
+      const res = await postSingUp(signUpData);
 
       if (res.status === 201) {
         alert("가입을 환영합니다! 로그인을 진행해 주세요.");
+        navigate("/login");
       }
     } catch (error) {
       console.log("회원가입 실패: ", error);
@@ -27,32 +38,20 @@ export default function SignUp() {
       <input
         type="text"
         placeholder="id"
-        onChange={(e) =>
-          setFormData({
-            ...formData,
-            userId: e.target.value,
-          })
-        }
+        name="userId"
+        onChange={handleChange}
       />
       <input
         type="text"
         placeholder="password"
-        onChange={(e) =>
-          setFormData({
-            ...formData,
-            password: e.target.value,
-          })
-        }
+        name="password"
+        onChange={handleChange}
       />
       <input
         type="text"
         placeholder="nickname"
-        onChange={(e) =>
-          setFormData({
-            ...formData,
-            nickname: e.target.value,
-          })
-        }
+        name="nickname"
+        onChange={handleChange}
       />
       <button type="submit">가입하기</button>
     </form>

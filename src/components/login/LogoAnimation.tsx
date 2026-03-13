8633430f138d65b796b552a0386a3c59c8730a15
YeEun Kim <kye1115z@naver.com>
2026-03-13 20:24:22 +0900

    #22 docs: k리그 팀 로고 이름 변경 및 데이터로

---
+++ src/components/login/LogoAnimation.tsx
@@ -0,0 +1,8 @@
+export default function LogoAnimation() {
+  return (
+    <div>
+      LOGO 지나가는 자리예요
+      <div>dd</div>
+    </div>
+  );
+}
---
+++ src/data/fc_teams.ts
@@ -0,0 +1,68 @@
+export const FC_TEAMS = [
+  {
+    id: 1,
+    team: "부천FC1995",
+    location: "부천",
+    image: `@/assets/logos/부천.svg`,
+  },
+  {
+    id: 2,
+    team: "FC안양",
+    location: "안양",
+    image: `@/assets/logos/안양.svg`,
+  },
+  {
+    id: 3,
+    team: "광주FC",
+    location: "광주",
+    image: `@/assets/logos/광주.svg`,
+  },
+  {
+    id: 4,
+    team: "울산 HD FC",
+    location: "울산",
+    image: `@/assets/logos/울산.svg`,
+  },
+  {
+    id: 5,
+    team: "김천상무 FC",
+    location: "김천",
+    image: `@/assets/logos/김천.svg`,
+  },
+  {
+    id: 6,
+    team: "대전 하나 시티즌",
+    location: "대전",
+    image: `@/assets/logos/대전.svg`,
+  },
+  {
+    id: 7,
+    team: "전북 현대 모터스",
+    location: "전북",
+    image: `@/assets/logos/전북.svg`,
+  },
+  {
+    id: 8,
+    team: "포항 스틸러스",
+    location: "포항",
+    image: `@/assets/logos/포항.svg`,
+  },
+  {
+    id: 9,
+    team: "제주 SK FC",
+    location: "제주",
+    image: `@/assets/logos/제주.svg`,
+  },
+  {
+    id: 10,
+    team: "인천 유나이티드",
+    location: "인천",
+    image: `@/assets/logos/인천.svg`,
+  },
+  {
+    id: 11,
+    team: "강원FC",
+    location: "강원",
+    image: `@/assets/logos/강원.svg`,
+  },
+];

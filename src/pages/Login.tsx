b0db33a73b1a284d65e40ecd82896d99294d17b9
kye1115z <kye1115z@naver.com>
2026-03-16 21:35:37 +0900

    #26 feat: 로그인 화면에 회원가입 만들기

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,7 +4,7 @@ import Input from "@/components/common/Input";
 import LogoAnimation from "@/components/login/LogoAnimation";
 import Typography from "@/styles/common/Typography";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -36,41 +36,57 @@ export default function Login() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col items-center justify-center pt-29 pb-10 w-full h-screen
-      bg-secondary box-border overflow-x-hidden"
-    >
-      <LogoAnimation />
-      <div className="flex flex-col items-center mb-8">
-        <Typography variant="h1" color="text-background" className="mb-4">
-          로그인
-        </Typography>
-        <Typography variant="h4" color="text-disabledGray">
-          FC라이츄 회원 로그인
-        </Typography>
-      </div>
-      <div className="flex flex-col items-start w-125">
-        <div className="flex flex-col gap-8 w-full">
-          <Input
-            label="ID"
-            name="userId"
-            placeholder="아이디"
-            onChange={handleChange}
-          />
-          <Input
-            label="비밀번호"
-            name="password"
-            placeholder="비밀번호"
-            type="password"
-            onChange={handleChange}
-          />
+    <div className="bg-secondary box-border overflow-x-hidden min-h-screen flex flex-col items-center justify-center pt-29 pb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center w-full h-full"
+      >
+        <LogoAnimation />
+        <div className="flex flex-col items-center mb-8">
+          <Typography variant="h1" color="text-background" className="mb-4">
+            로그인
+          </Typography>
+          <Typography variant="h4" color="text-disabledGray">
+            FC라이츄 회원 로그인
+          </Typography>
         </div>
 
-        <Button type="submit" width="fixed" className="mt-8">
-          로그인
-        </Button>
-      </div>
-    </form>
+        <div className="flex flex-col items-start w-125">
+          <div className="flex flex-col gap-8 w-full">
+            <Input
+              label="ID"
+              name="userId"
+              placeholder="아이디"
+              onChange={handleChange}
+            />
+            <Input
+              label="비밀번호"
+              name="password"
+              placeholder="비밀번호"
+              type="password"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="flex justify-between items-end w-full mt-4">
+            <Button type="submit" width="fixed" className="mt-8 mr-8">
+              로그인
+            </Button>
+            <Link
+              to="/signup"
+              className="flex gap-2 hover:opacity-80 transition-opacity"
+            >
+              <Typography
+                variant="body-md"
+                color="text-textSub"
+                className="hover:underline hover:text-background transition-colors "
+              >
+                회원가입
+              </Typography>
+            </Link>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
